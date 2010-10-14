5306bea3c7b8bfca25dceb41a37c71bcae453851
brandon <brandon@bethel-churchs-imac.local>
2010-10-14 16:47:01 -0700

    Stuff

---
+++ application/logs/2010-10-14.log.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2010-10-14 08:06:55 -07:00 --- error: Uncaught Kohana_Database_Exception: There was an SQL error: Table 'store.themes' doesn't exist - SHOW COLUMNS FROM `themes` in file system/libraries/drivers/Database/Mysql.php on line 371
