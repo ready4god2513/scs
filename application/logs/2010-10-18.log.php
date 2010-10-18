dad979d68760cf52ffabc75bcf71bbbef749070e
brandon <brandon@bethel-churchs-imac.local>
2010-10-18 10:11:32 -0700

    Stuff

---
+++ application/logs/2010-10-18.log.php
@@ -0,0 +1,4 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2010-10-18 10:10:36 -07:00 --- error: Uncaught Kohana_Exception: The requested view, themes//application, could not be found in file system/core/Kohana.php on line 1162
+2010-10-18 10:10:39 -07:00 --- error: Uncaught Kohana_Exception: The requested view, themes//application, could not be found in file system/core/Kohana.php on line 1162
